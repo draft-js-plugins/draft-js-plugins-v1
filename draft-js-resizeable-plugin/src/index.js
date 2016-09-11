7d94e765978974eeb932bd00d8aa05204529868e
Nik Graf <nik@nikgraf.com>
2016-09-12 01:18:30 +0200

    remove all non-v1 plugins

--- draft-js-resizeable-plugin/src/index.js
+++
@@ -1,6 +0,0 @@
-import Decorator from './decorators/resizeable';
-
-const resizeablePlugin = () => ({ });
-
-export default resizeablePlugin;
-export const ResizeableDecorator = options => Decorator(options);
